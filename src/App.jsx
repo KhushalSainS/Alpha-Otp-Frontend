c989df78ca34c362c28855dfba7cfe49237f134e
Lordshiva12 <khushalsains735@gmail.com>
2025-03-02 09:47:34 +0530

    update Navbar component placement and modify index.html title and favicon

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ function App() {
     <StoreContextProvider>
       <Router>
         <div className="min-h-screen bg-gray-50">
-          <Navbar />
+        
           <main className="py-6">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
               <Routes>
